5ec75379b1166a30f0b37ca6a6f85819b2d6cf9a
terced-leonardoo <terced-leonardoo@nm-thiago-mac.local>
2017-08-02 18:33:26 -0300

    DoubleClick para ativar o Draft

--- src/BoxEditor.js
+++ src/BoxEditor.js
@@ -22,12 +22,38 @@ export default class BoxEditor extends React.Component {
 
     this.state = {
       editorState: editorState,
+      readOnly: true,
       content: ''
     };
   }
 
+  _onDoubleClick = (event) => {
+    event.preventDefault();
+    
+    if(this.state.readOnly) {
+      this.setState({readOnly: false});
+      
+      setTimeout(() => {
+        this._editor.focus();
+      }, 250);
+    }
+  }
+
   _onChange = (editorState) => {
+    let editor = this._cont
+                  .children[0]
+                  .querySelector('.public-DraftEditor-content')
+                  .children[0];
+
+    console.log(editor.childNodes[0]);
+
     this.setState({ editorState });
+  }
+
+  _onBlur = (event) => {
+    setTimeout(() => {
+      this.setState({readOnly: true});
+    }, 250);
   }
 
   content = () => JSON.stringify(convertToRaw(this.state.editorState.getCurrentContent()));
@@ -44,10 +70,16 @@ export default class BoxEditor extends React.Component {
     return (
       <div
         className="box"
-        style={style}>
+        style={style}
+        onDoubleClick={this._onDoubleClick}
+        ref={node => this._cont = node}>
         <Editor
+          ref={node => this._editor = node}
           editorState={this.state.editorState}
+          readOnly={this.state.readOnly}
           onChange={this._onChange}
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
         />
       </div>
     );
